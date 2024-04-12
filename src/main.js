fab4816d49944c282d2ac91b6763fe83133dd509
tina3008 <kicja_3@ukr.net>
2024-04-12 22:20:40 +0300

    fix modal

--- src/main.js
+++ src/main.js
@@ -1,13 +1,10 @@
 import './js/api';
 import './js/reviews-main';
 import './js/review-swiper';
-
 import './js/faq-accordion';
 
-
-
+import './js/scroll';
 import './js/header';
-
-
-
 import './js/express';
+
+
