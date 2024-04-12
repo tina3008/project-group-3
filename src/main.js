39991f89f1baf5297195083a57a828de90477d95
Oksana Tanashchuk <oksana.tanashuk@gmail.com>
2024-04-12 23:55:32 +0200

    fix-my-fk

--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ import './js/api';
 import './js/reviews-main';
 import './js/review-swiper';
 
-import './js/faq-accordion';
-
 import './js/header';
 
+import './js/api';
 
+import './js/faq-accordion';
+
+import './js/scroll';
+import './js/header';
 
 import './js/service-worker';
