0bb79ed5f71ddb8838d18b9ea4e8a43d375a0957
Oksana Tanashchuk <oksana.tanashuk@gmail.com>
2024-04-12 23:39:07 +0200

    fix

--- src/main.js
+++ src/main.js
@@ -2,19 +2,8 @@ import './js/api';
 import './js/reviews-main';
 import './js/review-swiper';
 
-import './js/header'
-
-import './js/api';
-
 import './js/faq-accordion';
 
-
-
-
-
-import './js/scroll';
 import './js/header';
-import './js/express';
 
-
-
+import './js/service-worker';
