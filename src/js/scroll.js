6724e0b60f9a80098f9034a55d3575ac9c8e71c7
tina3008 <kicja_3@ukr.net>
2024-04-12 23:32:37 +0300

    open modal

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,8 +1,14 @@
 // modal window ++++++++++++++
 const modalWindow = document.querySelector("#modal-open");
 const modalBtn = document.querySelector("#modalBtn");
+const openModal = document.querySelector("#send-button");
 
 modalBtn.addEventListener("click", (e) => {
   e.preventDefault();
   modalWindow.classList.remove("is-open");
+});
+
+openModal.addEventListener("click", (e) => {
+  e.preventDefault();
+  modalWindow.classList.add("is-open");
 });
