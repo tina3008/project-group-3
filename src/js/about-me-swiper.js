8c7760f93045b81862aedfa7e76f2b70240c1d3e
BohdanLysniak <marttelo@ukr.net>
2024-04-14 14:58:36 +0300

    innit-swipper

--- src/js/about-me-swiper.js
+++ src/js/about-me-swiper.js
@@ -1,2 +1,12 @@
 import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
 import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+const aboutMeSwiperBox = document.querySelector(".swipper-and-button-wraper > .swiper");
+const aboutMeNextButton = document.querySelector('.swiper-next-button');
+
+const swiper = new Swiper(aboutMeSwiperBox, {
+  modules: [Navigation, Pagination],
+});
