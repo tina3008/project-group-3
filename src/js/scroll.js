9eb6fc8265fe606721e3e1edf9c95f3fe2a7d9e1
tina3008 <kicja_3@ukr.net>
2024-04-15 07:30:34 +0300

    smooth scroll

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,41 +1,13 @@
-// modal window ++++++++++++++
-// const modalWindow = document.querySelector("#modal-open");
-// const modalBtn = document.querySelector("#modalBtn");
-// const openModal = document.querySelector("#send-button");
-// const sendForm = document.querySelector(".footer-form");
 
-// modalBtn.addEventListener("click", (e) => {
-//   e.preventDefault();
-//   modalWindow.classList.remove("is-open");
-// });
+const smoothLinks = document.querySelectorAll('a[href^="#"]');
+for (let smoothLink of smoothLinks) {
+    smoothLink.addEventListener('click', e=> {
+        e.preventDefault();
+        const id = smoothLink.getAttribute('href');
 
-// sendForm.addEventListener("submit", (e) => {
-//   modalWindow.classList.add("is-open");
-// });
-
-// scroll
-
-// scroll benefit-footer
-
-const linkBenefits = document.querySelector('.benefit-footer');
-const footerSection = document.querySelector('#about');
-
-linkBenefits.addEventListener('click', e=> {
-    e.preventDefault();
-   
-    footerSection.scrollIntoView({behavior: "smooth", block: "center", inline: "start"});
-});
-
-// scroll from header
-
-const headerMenu = document.querySelector('#dropdown');
-const toBenefit = document.querySelector('#benefit-link');
-
-
-
-headerMenu.addEventListener('click', e=> {
-    e.preventDefault();
-    const memuChoice = e.target.element;
-  alert (memuChoice)
-    toBenefit.scrollIntoView({behavior: "smooth", block: "center", inline: "start"});
-});
+        document.querySelector(id).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+};
