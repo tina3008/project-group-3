12a2a9a8a65f4a54182be4d6c72d8d852a455286
tina3008 <kicja_3@ukr.net>
2024-04-16 21:15:33 +0300

    Update scroll.js

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,8 +1,12 @@
 
+const mobileWindow = document.querySelector('#mobail-menu');
 const smoothLinks = document.querySelectorAll('a[href^="#"]');
+
 for (let smoothLink of smoothLinks) {
     smoothLink.addEventListener('click', e=> {
         e.preventDefault();
+        mobileWindow.classList.remove('is-open');
+
         const id = smoothLink.getAttribute('href');
 
         document.querySelector(id).scrollIntoView({
