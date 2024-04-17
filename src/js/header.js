632b5e9af8c666de1cf1e205c15b6228345b7bda
Dimass1515 <di.tsvigun@gmail.com>
2024-04-17 15:00:46 +0300

    update menu

--- src/js/header.js
+++ src/js/header.js
@@ -7,19 +7,53 @@ document.addEventListener("DOMContentLoaded", function() {
   // Добавляем обработчик клика на ссылку меню
   profileLink.addEventListener("click", function(event) {
     event.preventDefault();
-    document.getElementById("profile").classList.toggle("active");
-  
-    const dropdown = document.getElementById("dropdown");
-    if (dropdown.classList.contains("active")) {
-      dropdown.classList.remove("active");
-      dropdown.style.opacity = 0;
-    } else {
-      dropdown.classList.add("active");
-      setTimeout(() => {
-        dropdown.style.opacity = 1;
-      }, 10); 
+    toggleMenu();
+  });
+
+  // Добавляем обработчик клика на все пункты меню
+  menuLinks.forEach(function(link) {
+    link.addEventListener("click", function(event) {
+      hideMenu();
+    });
+  });
+
+  // Добавляем обработчик клика на всё меню
+  dropdown.addEventListener("click", function(event) {
+    hideMenu();
+  });
+
+  // Добавляем обработчик клика на всё окно документа
+  document.addEventListener("click", function(event) {
+    const target = event.target;
+    // Проверяем, является ли цель клика частью меню или не её частью
+    if (!dropdown.contains(target) && !profileLink.contains(target)) {
+      hideMenu();
     }
   });
+});
+
+// Функция для открытия/закрытия меню
+function toggleMenu() {
+  const profile = document.getElementById("profile");
+  const dropdown = document.getElementById("dropdown");
+  profile.classList.toggle("active");
+  dropdown.classList.toggle("active");
+  if (dropdown.classList.contains("active")) {
+    dropdown.style.opacity = 1;
+  } else {
+    dropdown.style.opacity = 0;
+  }
+}
+
+// Функция для закрытия меню
+function hideMenu() {
+  const profile = document.getElementById("profile");
+  const dropdown = document.getElementById("dropdown");
+  profile.classList.remove("active");
+  dropdown.classList.remove("active");
+  dropdown.style.opacity = 0;
+}
+
 
   const menuToggleMobile = document.querySelector('.header-mobile-menu-open-btn');
 const menuToggleDesktop = document.getElementById('profile-link'); // Assuming this triggers desktop menu
