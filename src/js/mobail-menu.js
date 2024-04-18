7fefca17cd2aeac937467c0b34742b6281dc5d0e
tina3008 <kicja_3@ukr.net>
2024-04-18 09:57:01 +0300

    hiden body

--- src/js/mobail-menu.js
+++ src/js/mobail-menu.js
@@ -2,17 +2,18 @@
 const mobileWindow = document.querySelector('#mobail-menu');
 const mobailBtn = document.querySelector('#mobiail-btn');
 const burgerBtn = document.querySelector('#burger');
-const bodyHiden = document.querySelector('.body');
+const bodyHiden = document.querySelector('body');
 
 burgerBtn.addEventListener('click', e => {
   e.preventDefault(); 
-  bodyHiden.classList.toggle('visually-hidden');
+  bodyHiden.classList.toggle('.visually-hidden');
   mobileWindow.classList.toggle('is-open');
 });
 
 mobailBtn.addEventListener('click', e => {
   e.preventDefault();
 
-  bodyHiden.classList.toggle('visually-hidden');
+  bodyHiden.classList.remove('visually-hidden');
   mobileWindow.classList.toggle('is-open');
+
 });
