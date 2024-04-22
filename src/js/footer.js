3078770e43df41c94b61b4180f03069f16ef34ac
tina3008 <147944110+tina3008@users.noreply.github.com>
2024-04-22 21:25:09 +0300

    Revert "rewrite footer POST request"

--- src/js/footer.js
+++ src/js/footer.js
@@ -2,45 +2,37 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
 import { fetchRequests } from "./api";
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
 const footerButton = document.querySelector(".footer-button");
 const closeButton = document.querySelector(".click-js");
 const modalWindow = document.querySelector("[modal-open]");
 const footerForm = document.querySelector(".footer-form");
 
-async function getRequests(event) {
-    event.preventDefault();
-    const footerEmail = document.querySelector(".footer-email");
-    const footerComment = document.querySelector(".footer-comment");
-    let email = footerEmail.value;
-    let comment = footerComment.value;
+async function getRequests() {
     try {
-        const requests = await fetchRequests({ email, comment });
-        requests
-            .then((value) => {
-                footerButton.addEventListener('click', toggleModal);
-                closeButton.addEventListener('click', toggleModal);
-                modalWindow.addEventListener('click', removeModal);
-                function removeModal(e) {
-                    if (e.target === e.currentTarget) {
-                        modalWindow.classList.add('is-hidden');
-                    }
-                }
-
-                function toggleModal() {
-                    modalWindow.classList.toggle('is-hidden');
-                    document.body.classList.toggle('no-scroll');
-                }
-                footerForm.reset();
-            })
-            .catch((error) => {
-                iziToast.error({
-                    color: 'red',
-                    message: '❌ Sorry, there is an error. Please try again later!',
-                    position: 'topRight',
-                });
+        const requests = await fetchRequests();
+        const totalItems = requests.length;
+        if (totalItems === 0) {
+            iziToast.error({
+                color: 'red',
+                message: '❌ Sorry, there is an error. Please try again later!',
+                position: 'topRight',
             });
+        } else {
+            footerButton.addEventListener('click', toggleModal);
+            closeButton.addEventListener('click', toggleModal);
+            modalWindow.addEventListener('click', removeModal);
+            function removeModal(e) {
+                if (e.target === e.currentTarget) {
+                    modalWindow.classList.add('is-hidden');
+                }
+            }
+
+            function toggleModal() {
+                modalWindow.classList.toggle('is-hidden');
+                document.body.classList.toggle('no-scroll');
+            }
+            footerForm.reset();
+        }
     } catch (error) {
         // iziToast.error({
         //     color: 'red',
@@ -50,7 +42,7 @@ async function getRequests(event) {
     }
 }
 
-footerButton.addEventListener('submit', getRequests)
+getRequests()
 
 document.addEventListener('DOMContentLoaded', function () {
     const emailInput = document.getElementById('user-email');
